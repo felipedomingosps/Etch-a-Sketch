ecaa5665787e34380c9969bdadb86f08a4411f5a
Felipe Domingos <Felipedomingosps@gmail.com>
2022-08-15 21:27:33 -0300

    Create addGrid() function

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+
+function addGrid(quantity) {
+    let style = document.createElement('style');
+    style.innerHTML = `
+        .gridDisplay {
+            display: grid;
+            grid-template-columns: repeat(${quantity}, 1fr);
+            grid-template-rows: repeat(${quantity}, 1fr);
+        }
+    `;
+
+    let script = document.querySelector('script');
+    script.parentNode.insertBefore(style, script);
+}
+
 function addGridElement(quantity) {
     const gridDisplay = document.getElementById('gridDisplay');
     const divArray = [];
 
-    for (let i = 0; i < quantity; i++) {
+    for (let i = 0; i < (quantity*quantity); i++) {
         let div = document.createElement('div');
         div.classList.add('gridElement')
         divArray.push(div);
     }
+
+    gridDisplay.classList.add('gridDisplay');
 
     divArray.forEach((item) => {
         gridDisplay.appendChild(item);
